3497ace8d72be33b1fa49a8c15ee9bb87b57615c
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2020-03-22 19:10:49 -0500

    made port lazy loaded

--- src/components/PortImage.js
+++ src/components/PortImage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Img from 'gatsby-image'
 
 import styles from '../sass/port-image.module.scss'
 
@@ -6,7 +7,10 @@ const PortImage = ({ port }) => {
   return (
     <div className={styles.container}>
       <a href={port.link} className={styles.link}>
-        <img src={port.image} className={styles.image} alt={port.alt} />
+        <div className={styles.image} alt={port.alt}>
+          {port.image}
+        </div>
+        {/* <img src={port.image} className={styles.image} alt={port.alt} /> */}
         <div styles={styles.overlay}>
           <h2 className={styles.title}>{port.title}</h2>
         </div>
--- src/constants/portfolio.js
+++ src/constants/portfolio.js
@@ -1,75 +1,80 @@
-import jsConf from '../images/jsConf.jpg'
-import crownClothing from '../images/crownClothing.jpg'
-import turtleTroupeThumbnail from '../images/turtletroupethumbnail.png'
+import React from 'react'
+
 import turtleTroupeStationary from '../images/turtleTroupeStationary.jpg'
-import karrayT from '../images/karraythumbnail.jpg'
+
 import karrays from '../images/karrays.jpg'
-import gitPeeps from '../images/gitpeeps.png'
-import logger from '../images/logger.png'
-import sfReality from '../images/sfreality.png'
-import travel from '../images/travelalps.jpg'
-import novoThumbnail from '../images/novothumbnail.jpg'
+
 import novoStationary from '../images/novostationary.jpg'
+
+import ImageJs from '../components/ImageJS'
+import ImageAlps from '../components/ImageAlps'
+import ImageCrown from '../components/ImageCrown'
+import ImageGit from '../components/ImageGit'
+import ImageKarray from '../components/ImageKarray'
+import ImageLogger from '../components/ImageLogger'
+import ImageNovo from '../components/ImageNovo'
+import ImageSf from '../components/ImageSf'
+import ImageTurtle from '../components/ImageTurtle'
 
 export default [
   {
-    image: jsConf,
+    image: <ImageJs />,
     title: 'JS Conference',
     text: 'UI',
     link: 'https://zealous-nobel-933483.netlify.com',
     alt: 'JS Conference',
   },
   {
-    image: crownClothing,
+    image: <ImageCrown />,
     title: 'Crown Clothing',
     text: 'UI/React',
     link: 'https://dt-clothing-site.herokuapp.com/',
     alt: 'Clothing Site',
   },
   {
-    image: travel,
+    image: <ImageAlps />,
     title: 'Travel Alps',
     text: 'UI',
     link: 'https://focused-sammet-b22f4d.netlify.com/',
     alt: 'Travel Site',
   },
   {
-    image: gitPeeps,
+    image: <ImageGit />,
     title: 'GitHub Searcher',
     text: 'UI/React',
     link: 'https://elated-bardeen-a61c26.netlify.com/',
     alt: 'GitHub Searcher',
   },
   {
-    image: sfReality,
+    image: <ImageSf />,
     title: 'SF Reality',
     text: 'UI',
     link: 'https://condescending-raman-495eed.netlify.com/',
     alt: 'Reality Site',
   },
   {
-    image: logger,
+    image: <ImageLogger />,
     title: 'System Logger',
     text: 'UI/React',
     link: 'https://shrouded-sierra-84061.herokuapp.com/',
     alt: 'System Logger',
   },
   {
-    image: novoThumbnail,
+    image: <ImageNovo />,
     title: 'Novo',
     text: 'Graphic Design',
     link: novoStationary,
     alt: 'Novo',
   },
   {
-    image: turtleTroupeThumbnail,
+    image: <ImageTurtle />,
     title: 'Turtle Troupe',
     text: 'Graphic Design',
     link: turtleTroupeStationary,
     alt: 'Turtle Troupe',
   },
   {
-    image: karrayT,
+    image: <ImageKarray />,
     title: "Karray's",
     text: 'Graphic Design',
     link: karrays,
