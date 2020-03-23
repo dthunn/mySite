f3c0ae8482dc7596504e6f359ea8ce039ebc87bf
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2020-03-23 14:13:00 -0500

    added for analytics features

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: 'UA-144966630-1',
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+        pageTransitionDelay: 0,
       },
     },
     {
